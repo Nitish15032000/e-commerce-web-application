c7877c3c30f912a653fca98f3736e84608a12f7d
Nitish kumar <nitish.edu.in@gmail.com>
2025-01-10 02:26:25 +0530

    contextprovider is read to use inside the app.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import ShopContextPovider from "./context/ShopContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    {/* Wrap your app with the ShopContextPovider component */}
+    {/* Provide your shop data here */}
+    <ShopContextPovider>
+      <App />
+    </ShopContextPovider>
+    
+  </BrowserRouter>
+);
